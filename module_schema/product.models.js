14934df54abc4a9a1665417496a64e5ea1ede1c3
raj251099 <102846756+raj251099@users.noreply.github.com>
2022-05-20 07:09:38 -1100

    Create product.models.js

---
+++ module_schema/product.models.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const crypto =require("crypto");
+
+const productSchema =new mongoose.Schema({
+    uuid:{type:String,required:false},
+    productName:{type:String,required:true,trim:true},
+    description:{type:String,required:true,trim:true},
+    price:{type:Number,required:true,trim:true},
+    quantity:{type:Number, required:true},
+    userUuid:{type:String,requird:true,trim:true},
+    listUuid:{type:String,required:true,trim:true}
+},
+{
+    timestamps:true
+})
+
+productSchema.pre('save',function(next){
+    this.uuid= "prod_"+crypto.pseudoRandomBytes(7).toString("hex").toLowerCase()
+    console.log(this.uuid);
+    next();
+});
+module.exports=mongoose.model('product',productSchema,'product');
